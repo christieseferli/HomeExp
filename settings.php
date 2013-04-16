ffd2d8ba2396cc096039be568afd014a34893530
christie <christieseferli@gmail.com>
2013-04-16 22:01:21 +0200

    T003: same initialization file for live and stable

--- settings.php
+++ settings.php
@@ -1,8 +1,21 @@
 <?php
-$settings = array(
-    'db_username' => 'expenses',
-    'db_password' => 'S3f3rl1@@',
-    'db_name'     => 'expenses_db',
-    'site_url'    => 'http://www.christieseferli.com/expenses_stable'
-);
+
+$exploded = explode('/', $_SERVER['REQUEST_URI']);
+$env = $exploded[1];
+
+if ($env == 'homeexpdev') {
+    $settings = array(
+        'db_username' => 'christieseferli',
+        'db_password' => 'S3f3rl1@@',
+        'db_name' => 'christieseferli_db',
+        'site_url' => 'http://www.christieseferli.com/homeexpdev'
+    );
+} elseif($env == 'expenses_stable') {
+    $settings = array(
+        'db_username' => 'expenses',
+        'db_password' => 'S3f3rl1@@',
+        'db_name' => 'expenses_db',
+        'site_url' => 'http://www.christieseferli.com/expenses_stable'
+    );
+}
 ?>
